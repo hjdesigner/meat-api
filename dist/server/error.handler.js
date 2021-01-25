547b1df2924025cfd1c4fd0dfa2970bb6716793d
Henrique Melanda <hjdesigner23@gmail.com>
2021-01-25 12:22:46 -0300

    standard mongooose validators

--- dist/server/error.handler.js
+++ dist/server/error.handler.js
@@ -2,6 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleError = void 0;
 const handleError = (req, resp, err, done) => {
+    Object.defineProperty(err, 'toJSON', {
+        value() {
+            const alt = {};
+            Object.getOwnPropertyNames(this).forEach(function (key) {
+                alt[key] = this[key];
+            }, this);
+            return alt;
+        },
+        configurable: true,
+        writable: true
+    });
     err.toJSON = () => {
         return {
             message: err.message
@@ -15,6 +26,13 @@ const handleError = (req, resp, err, done) => {
             break;
         case 'ValidationError':
             err.statusCode = 400;
+            const messages = [];
+            for (let name in err.errors) {
+                messages.push({ message: err.errors[name].message });
+            }
+            err.toJSON = () => ({
+                errors: messages
+            });
             break;
     }
     done();
--- server/error.handler.ts
+++ server/error.handler.ts
@@ -1,20 +1,40 @@
 import * as restify from 'restify'
 
-export const handleError = (req: restify.Request, resp: restify.Response, err, done) => {
+export const handleError = (req: restify.Request, resp: restify.Response, err, done)=>{
+  Object.defineProperty(err, 'toJSON', {
+    value() {
+      const alt = {}
+ 
+      Object.getOwnPropertyNames(this).forEach(function (key) {
+        alt[key] = this[key]
+      }, this)
+      return alt
+    },
+    configurable: true,
+    writable: true
+  })
   err.toJSON = () => {
     return {
-      message: err.message
+      message : err.message
     }
   }
-  switch(err.name) {
+  switch(err.name){
     case 'MongoError':
-      if (err.code === 11000) {
+      if(err.code === 11000){
         err.statusCode = 400
       }
       break
     case 'ValidationError':
       err.statusCode = 400
+      const messages: any[] = []
+      for(let name in err.errors){
+        messages.push({message: err.errors[name].message})
+      }
+      err.toJSON = () => ({
+        errors: messages
+      })
       break
   }
   done()
 }
+
